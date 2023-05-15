baf2c5b9fb60c2f1d6dbc321ca65eb51a057c749
danil <natalya.haritonov.72@gmail.com>
2023-05-15 19:45:36 +0300

    two sums complete

--- src/main/kotlin/_1_Two_Sums/two_sums.kt
+++ src/main/kotlin/_1_Two_Sums/two_sums.kt
@@ -1,17 +1,22 @@
 package _1_Two_Sums
 
+
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val map = mutableMapOf<Int, Int>()
-        for (i in nums)
-        {
-            var j = nums[i] - target
-
+        val map: MutableMap<Int, Int> = HashMap()
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (map.containsKey(complement)) {
+                return intArrayOf(map[complement]!!, i)
+            }
+            map[nums[i]] = i
         }
+        return intArrayOf(-1, -1)
     }
 }
 
 fun main()
 {
-
+    val answer = Solution()
+    answer.twoSum(intArrayOf(3,54,5,), 3)
 }
