40e0fb19ee342d083868a8acb192b993ad54f04a
danil <natalya.haritonov.72@gmail.com>
2023-05-15 19:08:51 +0300

    two sums

---
+++ src/main/kotlin/_1_Two_Sums/two_sums.kt
@@ -0,0 +1,17 @@
+package _1_Two_Sums
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+        for (i in nums)
+        {
+            var j = nums[i] - target
+
+        }
+    }
+}
+
+fun main()
+{
+
+}
